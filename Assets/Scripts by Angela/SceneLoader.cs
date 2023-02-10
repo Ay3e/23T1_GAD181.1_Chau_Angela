160ffacc6a5602534002c5b4d315e2a1856379e4
Ay3e <angela.chau@students.mq.edu.au>
2023-02-11 08:58:42 +1100

    Organized hierarchy on all scenes and adding functioning buttons
    - make changes in the SceneLoader Script to have a working play, retry and to main menu buttons

--- Assets/Scripts by Angela/SceneLoader.cs
+++ Assets/Scripts by Angela/SceneLoader.cs
@@ -2,23 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 namespace AngelaChau {
     public class SceneLoader : MonoBehaviour
     {
+        private int mainMenu = 0;
+        private int levelOne = 1;
+        private int loseScene = 2;
+        private int winScene = 3;
+        private int restartCurrentTime = 10;
+        private int restartobjectsRemaining = 4;
         private void Update()
         {
             //If CurrentTime is 0 then change to LOSE scene
             if (Countdown.currentTime == 0)
             {
                 //Scene 1 is LoseScene
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(loseScene);
+                ClickMechanic.objectsRemaining = 4;
+                Countdown.currentTime = restartCurrentTime;
             }
             //If objectsRemaining is 0 then change to WIN scene
             if (ClickMechanic.objectsRemaining == 0)
             {
                 //Scene 2 is WinScene
-                SceneManager.LoadScene(2);
+                SceneManager.LoadScene(winScene);
+                ClickMechanic.objectsRemaining = 4;
+                Countdown.currentTime = restartCurrentTime;
             }
+        }
+        //If button PLAY is pressed go to levelOne
+        public void ButtonToPlay()
+        {
+            SceneManager.LoadScene(levelOne);
+        }
+        //If button MAIN MENU is pressed go to main menu scene
+        public void ButtonToMainMenu()
+        {
+            SceneManager.LoadScene(mainMenu);
+        }
+        //If button RETRY is pressed go to levelOne
+        public void ButtonToRetry()
+        {
+            SceneManager.LoadScene(levelOne);
         }
     }
 }
